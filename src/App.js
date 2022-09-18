7b05a1afa4d78af9a3d1a4806d92d8771583117b
AnDamazio <damazioandre@hotmail.com>
2022-09-18 14:38:50 -0300

    Add mostar wars api connection

--- src/App.js
+++ src/App.js
@@ -1,31 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [loadingProgress, setLoadingProgress] = useState("");
+
+  const loadAnimation = useEffect(() => {
+    const timer = setInterval(() => {
+      if (isLoading && loadingProgress.length < 3) {
+        setLoadingProgress((prev) => prev.concat("."));
+      } else if (isLoading && loadingProgress.length === 3) {
+        setLoadingProgress("");
+      }
+
+      if (!isLoading) {
+        setLoadingProgress("");
+      }
+    }, 100);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [loadingProgress, isLoading]);
+
+  async function fetchMoviesHandler() {
+    setIsLoading(true);
+    const response = await fetch("https://swapi.dev/api/films");
+    const data = await response.json();
+
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+    setMovies(transformedMovies);
+    setIsLoading(false);
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {isLoading && <p>Loading {loadingProgress}</p>}
       </section>
     </React.Fragment>
   );
